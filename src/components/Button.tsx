f0a570bececd35405396cd42716549d74619717e
William Hideki Nakata <whnakata@gmail.com>
2023-07-08 13:58:07 -0300

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,7 +12,7 @@ const Button: React.FC<ButtonType> = ({ href, children, className }) => {
       className={
         className +
         " flex items-center justify-center rounded border border-emphasis-300 bg-transparent px-4 py-3 font-mono text-emphasis-300 outline-none transition max-lg:px-3 max-lg:py-2 max-lg:text-sm " +
-        "-translate-x-[4px] -translate-y-[4px] bg-primary-100/10 shadow-[3px_3px_0_0] hover:shadow-emphasis-300 active:-translate-x-[2px] active:-translate-y-[2px] active:shadow-[0px_0px_0_0]"
+        "-translate-x-[2px] -translate-y-[2px] bg-primary-100/10 shadow-[3px_3px_0_0] hover:shadow-emphasis-300 active:-translate-x-[0px] active:-translate-y-[0px] active:shadow-[0px_0px_0_0]"
       }
     >
       {children}
