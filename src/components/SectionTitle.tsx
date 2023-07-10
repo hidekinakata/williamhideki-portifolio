2fe116f113ed3ba1be8151590b9991ba9fca7049
William Hideki Nakata <whnakata@gmail.com>
2023-07-10 10:38:03 -0300

    Create SectionTitle.tsx

---
+++ src/components/SectionTitle.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+type SectionTitleType = {
+  sectionName: string;
+};
+
+const SectionTitle: React.FC<SectionTitleType> = ({ sectionName }) => {
+  return (
+    <h1
+      className={
+        "relative mb-10 mt-3 flex w-[60%] items-center justify-end gap-4 place-self-end whitespace-nowrap text-3xl max-lg:w-full " +
+        "before:block before:h-0.5 before:w-full before:bg-white/10 before:content-[''] "
+      }
+    >
+      {sectionName}
+      <span
+        className={"absolute top-0 -translate-y-3/4 text-sm text-emphasis-300"}
+      >
+        section
+      </span>
+    </h1>
+  );
+};
+
+export default SectionTitle;
